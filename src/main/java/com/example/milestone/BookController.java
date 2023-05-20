74d2d6c2743b8d5ad6c6586e8747bbbd13a1b0c7
hyperkinoko <github.hyperkinoko@kinokodata.net>
2023-05-20 17:13:42 +0900

    post終わり

--- src/main/java/com/example/milestone/BookController.java
+++ src/main/java/com/example/milestone/BookController.java
@@ -3,6 +3,8 @@ package com.example.milestone;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BookController {
@@ -16,5 +18,19 @@ public class BookController {
         books[3] = new Book("Java演習", "kinoshita", 45, 5);
         model.addAttribute("books", books);
         return "book-list";
+    }
+
+    @GetMapping("books/add")
+    public String getAddBook(Model model) {
+        model.addAttribute("input", new PostBookRequest());
+        return "book-add";
+    }
+
+    @PostMapping("books/add")
+    public String postAddBook(@ModelAttribute PostBookRequest input, Model model) {
+        Book book = new Book(input.getTitle(), input.getAuthor(), input.getNumOfPages(), input.getClassification());
+        System.out.println(book);
+        model.addAttribute("book", book);
+        return "result";
     }
 }
---
+++ src/main/java/com/example/milestone/PostBookRequest.java
@@ -0,0 +1,54 @@
+package com.example.milestone;
+
+public class PostBookRequest {
+    // 本のタイトル
+    private String title;
+
+    // 本の著者
+    private String author;
+    
+    // 本のページ数
+    private int numOfPages;
+    
+    // 本の分類番号
+    private int classification;    
+
+    public PostBookRequest() {
+        this.title = "";
+        this.author = "";
+        this.numOfPages = 0;
+        this.classification = 0;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+    
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    public String getAuthor() {
+        return this.author;
+    }
+    
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    public int getNumOfPages() {
+        return this.numOfPages;
+    }
+    
+    public void setNumOfPages(int numOfPages) {
+        this.numOfPages = numOfPages;
+    }
+    
+    public int getClassification() {
+        return this.classification;
+    }
+    
+    public void setClassification(int classification) {
+        this.classification = classification;
+    } 
+}
